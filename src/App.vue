6489b8dd9c8a1bc5f76ee0a7004100e6387b6ead
HealerK <shinemyatshwe@gmail.com>
2022-08-03 22:18:12 +0630

    reaction-timer finished

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1>Shine Reaction Timer</h1>
+    <button class="btn btn-success" @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <Results v-if="showResult" :score="score"/>
+  </div>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Result.vue";
+
+export default {
+  name: "App",
+  components: { Block,Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult : false,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showResult = false;
+    },
+    endGame(reactionTime){
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResult = true;
+    }
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,29 @@
+<template>
+    <p>Reaction Timer : {{ score }} ms</p>
+    <h5 class="text-bold fs-3 text-success">{{rank}}</h5>
+</template>
+
+<script>
+export default {
+    name: 'Result-vue',
+    props:['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+   mounted() {
+    if(this.score < 250){
+        this.rank = 'You have ninja finger'
+    }else if(this.score < 400){
+        this.rank = 'You are not bad!'
+    }else{
+        this.rank = 'You should try better!'
+    }
+   },
+}
+</script>
+
+<style>
+
+</style>
